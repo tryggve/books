5b6c1e947f1d996b2ad0dbb958ca42f048a80ab3
Tryggve Blom <tryggve@tryggve.nu>
2026-04-30 23:17:39 +0200

    Update css in book form to have checkboxes behave on safari

--- views/components/book-form.tsx
+++ views/components/book-form.tsx
@@ -8,9 +8,10 @@ const container = css`padding: 20px; width: 100%; min-height: 400px;`
 const header = css`display: flex; justify-content: space-between; align-items: center;`
 const bookForm = css`display: flex; flex-direction: column; gap: 1rem;`
 const label = css`display: block; font-size: 13px; color: var(--color-text-secondary); margin-bottom: 4px;`
-const inputSelect = css`padding: 10px 5px; width: 100%; border: 1px solid var(--color-border-secondary); border-radius: 8px;`
+const input = css`padding: 10px 5px; width: 100%; border: 1px solid var(--color-border-secondary); border-radius: 8px;`
+const select = css`border: 1px solid var(--color-border-secondary); border-radius: 8px;`
 const seriesWrapper = css`display: grid; grid-template-columns: 3fr 1fr; gap: 12px;`
-const checkboxWrapper = css`border-top: 0.5px solid var(--color-border-secondary); padding-top: 1rem; display: flex; gap: 1.5rem;`
+const checkboxWrapper = css`border-top: 0.5px solid var(--color-border-secondary); padding-top: 1rem; display: flex; gap: 16px;`
 const checkboxLabel = css`display: flex; align-items: center; gap: 8px; font-size: 14px; color: var(--color-text-primary); cursor: pointer;`
 const formSubmitWrapper = css`display: flex; gap: 8px; padding-top: 0.5rem;`
 const button = css`font-size: 12px; padding: 8px 16px; border-radius: 8px; border: 0.5px solid var(--color-border-secondary); background: transparent; color: var(--color-text-secondary); cursor: pointer; &:hover {background-color: var(--color-background-tertiary);}`
@@ -28,11 +29,11 @@ const BookForm: FC<PropsWithChildren<{closeButton: any}>> = ({closeButton}) => {
             <form id="book-form" method="post" action="/books" class={bookForm}>
                 <div>
                     <label for="book-title" class={label}>Titel</label>
-                    <input type="text" name="title" id="book-title" class={inputSelect} placeholder="Boktitel" required autofocus />
+                    <input type="text" name="title" id="book-title" class={input} placeholder="Boktitel" required autofocus />
                 </div>
                 <div>
                     <label for="book-author" class={label}>Författare</label>
-                    <select name="author" id="book-author" class={inputSelect} required>
+                    <select name="author" id="book-author" class={input} required>
                         <option value="">Välj</option>
                         { authors.map((a) => (<option value={a.id}>{a.name}</option>)) }
                     </select>
@@ -40,26 +41,26 @@ const BookForm: FC<PropsWithChildren<{closeButton: any}>> = ({closeButton}) => {
                 <div class={seriesWrapper}>
                     <div>
                         <label for="book-series" class={label}>Bokserie</label>
-                        <select name="series" id="book-series" class={inputSelect}>
+                        <select name="series" id="book-series" class={input}>
                             <option value="">Ingen serie</option>
                             { series.map((s) => (<option value={s.id} data-author-id={s.primaryAuthorId} data-next-part={s.nextPart}>{s.name}</option>)) }
                         </select>
                     </div>
                     <div>
                         <label for="series-order" class={label}>Del</label>
-                        <input type="number" name="order" id="series-order" class={inputSelect} placeholder="" min="0" oninput="validity.valid||(value='');" disabled />
+                        <input type="number" name="order" id="series-order" class={input} placeholder="" min="0" oninput="validity.valid||(value='');" disabled />
                     </div>
                 </div>
                 <div>
                     <label for="year-published" class={label}>Utgivningsår</label>
-                    <input type="number" name="year" id="year-published" class={inputSelect} placeholder="" min="0" oninput="validity.valid||(value='');" />
+                    <input type="number" name="year" id="year-published" class={input} placeholder="" min="0" oninput="validity.valid||(value='');" />
                 </div>
                 <div class={checkboxWrapper}>
-                    <label for="read-check" class={checkboxLabel}>
-                        <input type="checkbox" name="read" id="read-check" class={inputSelect} value="true" /> Läst
+                    <label class={checkboxLabel}>
+                        <input type="checkbox" name="read" id="read-check" class={select} value="true" /> Läst
                     </label>
-                    <label for="owned-check" class={checkboxLabel}>
-                        <input type="checkbox" name="owned" id="owned-check" class={inputSelect} value="true" /> Äger
+                    <label class={checkboxLabel}>
+                        <input type="checkbox" name="owned" id="owned-check" class={select} value="true" /> Äger
                     </label>
                 </div>
                 <div class={formSubmitWrapper}>
